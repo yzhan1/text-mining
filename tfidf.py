120f9c6e1dca6366ca1a165b249ebf23d321b4a0
Yaoming Zhan <zhanyaoming2014@gmail.com>
2017-08-06 16:10:01 +0800

    implemented tfidf algorithm

---
+++ tfidf.py
@@ -0,0 +1,48 @@
+import math
+import pprint
+
+title_hash = {}
+count_hash = {}
+num_of_words = 0
+num_of_docs = 0
+segmented_lst = []
+res = {}
+file = open('data/allType.txt', 'r').read()
+lst = file.splitlines()
+
+print('### Processing')
+for i, line in enumerate(lst):
+    line = line.split(' ')[1:]
+    title_hash[i] = line
+    num_of_docs += 1
+    segmented_lst.append(line)
+    for word in line:
+        if word == 'nbsp':
+            continue
+        if word in count_hash:
+            count_hash[word]['total'] += 1
+            if i in count_hash[word]:
+                count_hash[word][i] += 1
+            else:
+                count_hash[word][i] = 1
+        else:
+            count_hash[word] = {}
+            count_hash[word]['total'] = 1
+            count_hash[word][i] = 1
+        num_of_words += 1
+
+print('### Calculating TF-IDF values for documents')
+for i, lst in enumerate(segmented_lst):
+    vec = []
+    for word in lst:
+        tf_idf = 0.0
+        if word in count_hash:
+            if count_hash[word]['total'] >= 5:
+                tf = count_hash[word][i] / len(title_hash[i])
+                tidj = len(count_hash[word]) - 1
+                idf = math.log(num_of_docs / (1 + tidj))
+                tf_idf = tf * idf
+        vec.append(tf_idf)
+    res[i] = vec
+
+pprint.pprint(res)
